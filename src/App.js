c00202f2d99f64298878f25e7fe407f5d3fcbfbf
Pavlo1307 <pavloshavel@gmail.com>
2021-06-30 16:43:36 +0300

    reducer work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from "react";
 
-function App() {
+function reducer(state, action) {
+
+  switch (action.type) {
+    case 'INCREMENT_A':
+      return{...state, a: state.a + action.playload}
+    case 'INCREMENT_B':
+      return{...state, b: state.b + action.playload}
+  }
+}
+
+export default function App() {
+
+  let[state, dispatch] = useReducer(reducer, {a:0, b:0});
+
   return (
-    <div className="App">
-    </div>
+      <div>
+        <button onClick={() =>{dispatch({type:'INCREMENT_A', playload:1});} }>inc a  - {state.a}</button>
+        <br/>
+        <button onClick={() =>{dispatch({type:'INCREMENT_B', playload:2});} }>inc a  - {state.b}</button>
+
+      </div>
   );
 }
 
-export default App;
+

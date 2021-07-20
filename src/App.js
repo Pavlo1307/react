bf17a9d3f896fc3465ef4eb548efdb55831d371c
Pavlo1307 <pavloshavel@gmail.com>
2021-07-20 15:15:30 +0300

    something do

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import Users from './components/users/Users'
-import {BrowserRouter as Router, Link,Switch,Route} from 'react-router-dom';
+import {useSelector} from "react-redux";
+import {useState} from "react";
+
+
+const CreateTodosForm = (onSubmit) =>{
+
+    const [title, setTitle] = useState('');
+    const [descr, setDescr] = useState('');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        //console.log(title, descr)
+
+        if(!title  || !descr)
+
+        onSubmit(title, descr)
+
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder={'title'}
+                value={title}
+                onChange={({target:{value}})=>setTitle(value)}/>
+            <br/>
+            <br/>
+            <input
+                type="text"
+                placeholder={'descriptions'}
+                value={descr}
+                onChange={({target:{value}})=>setDescr(value)}/>
+            <br/>
+            <br/>
+            <button type="submit" disabled={!title || !descr}>create todo </button>
+        </form>
+    );
+};
 
 export default function App() {
-  return (
-      <div></div>
+    const store = useSelector(state=> state)
+    console.log(store)
+    const onTodoCreate = async (title, descr) =>{
+        if(!title  || !descr) return;
+
+        const response = await fetch('http://localhost:8888',{
+            method: 'POST',
+            body: JSON.stringify({title, descr})
+        })
+        const data = await response.json()
+        console.log(data)
+
+    }
+    return (
+      <div>
+          <CreateTodosForm onSubmit={onTodoCreate}/>
+      </div>
         );
 }
 
---
+++ src/redux/redusers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from "redux";
+import {counterReduser} from "./counterReduser";
+import {todosReduser} from "./todosReduser";
+
+
+export const rootReduser = combineReducers({
+    counterReduser,
+    todosReduser
+
+})
